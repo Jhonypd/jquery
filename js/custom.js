d358fa97ef23c6768b67ba4ceb54b2bbe388c5c9
Jhony Pereira <jhony-16@live.com>
2023-09-14 17:21:46 -0300

    adicionado o sitemap, feito atualizações de dependencias

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 // jQuery( function($){
 $(document).ready(function () {
 
-    $('.owl-carousel').owlCarousel();
+   /* $('.owl-carousel').owlCarousel();
 
     let titulos = $('h4') // tag
 
@@ -41,14 +41,112 @@ $(document).ready(function () {
     /*
      * Manipulação de eventos
      */
-    $('.featured-item a').on('blur', function (event) {
+    /*$('.featured-item a').on('blur', function (event) {
 
         event.preventDefault();
 
         alert('Produto esgotado');
 
-    })
+    })*/
+
+
+
+
+    /*
+     *Ouvinte de eventos .nav-modal-open
+    */
+
+     $('.nav-modal-open').on('click', function(e){
+
+        e.preventDefault();
+
+        let elem = $(this).attr('rel')
+
+        $('.modal-body').html($('#'+elem).html())
+
+        $('.modal-header h5.modal-title').html($(this).text())
+
+        let myModal = new bootstrap.Modal($('#modalId'))
+
+        myModal.show()
+
+     })
+
+
+     /*
+        * Validação de Formulário
+     */
+
+     function validate(elem){
+        if(elem.val() == ''){
+            console.log('O campo de '+ elem.attr('name') +' é obrigatório')
+            elem.parent().find('.text-muted').show()
+            elem.addClass('invalid')
+
+            return false
+        } else {
+            elem.parent().find('.text-muted').hide()
+            elem.removeClass('invalid')
+        }
+     }
+
+     $('body').on('submit', '.modal-body .form', function(e){
+        e.preventDefault()
+
+        const inputName = $('#nome')
+        const inputEmail = $('#email')
+
+        validate(inputName)
+        validate(inputEmail)
+
+        
+        if(inputEmail.hasClass('invalid') || inputName.hasClass('invalid')){
+            console.log('Verifique os campos obrigatrios')
+            return false
+        } else {
+            $(this).submit()
+        }
+     })
+
+     $('body').on('blur', '#nome', function (){
+        validate($(this))
+     })
+     $('body').on('blur', '#email', function (){
+        validate($(this))
+     })
+     $('body').on('blur', '#date', function (){
+        $(this).datepicker()
+        validate($(this))
+        $(this).mask('00/00/0000')
+     })
+     $('body').on('blur', '#hour', function (){
+        validate($(this))
+        $(this).mask('00:00')
+     })
+     $('body').on('keyup', '#cep', function (){
+        validate($(this))
+        $(this).mask('00000-000')
+     })
+     $('body').on('keyup', '#phone', function (){
+        validate($(this))
+        $(this).mask('(00) 00000-0000');
+     })
+     $('body').on('keyup', '#cpf', function (){
+        validate($(this))
+        $(this).mask('000.000.000-00')
+     })
+
+
+     
+
 })
+
+
+
+
+/* ********************** */
+
+/*btn top */
 
 const scrollTop = document.getElementById('scrolltop')
 
